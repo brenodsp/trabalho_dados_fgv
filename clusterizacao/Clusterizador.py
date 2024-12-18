0708f50c44eadf80c2af62d906cdebcf388a4e0e
Breno Pereira <brenodsp.rj@gmail.com>
2024-12-18 17:36:01 -0300

    [feat] Atribuir pesos aos grupos de métricas

--- clusterizacao/Clusterizador.py
+++ clusterizacao/Clusterizador.py
@@ -1,4 +1,5 @@
 from base_de_dados.DadosBase import DadosBase
+from coletores.enums import GrupoMetricasEnum
 
 import datetime
 
@@ -7,6 +8,9 @@ import sklearn.cluster as skl_cluster
 
 
 class Clusterizador:
+    _PESO_ENDIVIDAMENTO = 10
+    _PESO_LIQUIDEZ = 7
+    _PESO_ESCALA = 1
     def __init__(self, dados: DadosBase):
         self._dados = dados.df.set_index('NOME_EMPRESA')
 
@@ -14,7 +18,7 @@ class Clusterizador:
         print(f"[{datetime.datetime.now()}] Realizando clusterização com o algoritmo KMeans...")
 
         # Determinar DataFrame base
-        df = self._dados
+        df = self._classificar_grupos_de_metricas(self._dados)
 
         # Criar o modelo KMeans
         kmeans = skl_cluster.KMeans(n_clusters=num_clusters, random_state=42)
@@ -26,6 +30,21 @@ class Clusterizador:
         labels = kmeans.labels_
 
         # Adicionar os rótulos dos clusters ao DataFrame original
+        df = self._classificar_grupos_de_metricas(df, reverter=True)
         df['Cluster'] = labels
 
         return df
+
+    def _classificar_grupos_de_metricas(self, df: pd.DataFrame, reverter: bool = False) -> pd.DataFrame:
+        # Criar DataFrames distintos para cada grupo de métrica
+        if reverter:
+            df_endividamento = df.loc[:, GrupoMetricasEnum.ENDIVIDAMENTO.value].div(self._PESO_ENDIVIDAMENTO)
+            df_liquidez = df.loc[:, GrupoMetricasEnum.LIQUIDEZ.value].div(self._PESO_LIQUIDEZ)
+            df_escala = df.loc[:, GrupoMetricasEnum.ESCALA.value].div(self._PESO_ESCALA)
+        else:
+            df_endividamento = df.loc[:, GrupoMetricasEnum.ENDIVIDAMENTO.value] * self._PESO_ENDIVIDAMENTO
+            df_liquidez = df.loc[:, GrupoMetricasEnum.LIQUIDEZ.value] * self._PESO_LIQUIDEZ
+            df_escala = df.loc[:, GrupoMetricasEnum.ESCALA.value] * self._PESO_ESCALA
+
+        # Agrupar novamente os DataFrames
+        return pd.concat([df_endividamento, df_liquidez, df_escala], axis=1)
