d62587fa8239aee00d1525f80458cdab61ca6ddd
Breno Pereira <brenodsp.rj@gmail.com>
2024-12-18 01:59:36 -0300

    [refactor] Adicionar rotina de escrita da base de dados em CSV e checagem de base de dados já existente

--- base_de_dados/DadosBaseYahoo.py
+++ base_de_dados/DadosBaseYahoo.py
@@ -2,6 +2,7 @@ from .DadosBase import DadosBase
 from coletores import ColetorFinanceiroYahoo
 
 import datetime
+import os
 
 import pandas as pd
 
@@ -9,28 +10,31 @@ import pandas as pd
 class DadosBaseYahoo(DadosBase):
     """
     Todas as empresas do S&P500 (17/12/2024).
+
+    Obs.: Exceto empresas sem EBITDA (BRK-B, JPN, BAC, WFC, MS, C, BK, GS) -> Instituições financeiras.
+    Obs.: Empresas com menos de 5 anos de dados de preços (KVUE).
     """
     EMPRESAS = [
-        "AAPL", "MSFT", "NVDA", "AMZN", "META", "TSLA", "AVGO", "GOOGL", "GOOG", "BRK.B",
-        "JPM", "LLY", "V", "XOM", "UNH", "COST", "MA", "WMT", "HD", "PG", "NFLX", "JNJ",
-        "CRM", "BAC", "ABBV", "ORCL", "CVX", "MRK", "WFC", "KO", "CSCO", "NOW", "ACN",
+        "AAPL", "MSFT", "NVDA", "AMZN", "META", "TSLA", "AVGO", "GOOGL",
+        "LLY", "V", "XOM", "UNH", "COST", "MA", "WMT", "HD", "PG", "NFLX", "JNJ",
+        "CRM", "ABBV", "ORCL", "CVX", "MRK", "KO", "CSCO", "NOW", "ACN",
         "PEP", "MCD", "IBM", "LIN", "AMD", "ADBE", "DIS", "TMO", "ABT", "PM", "ISRG",
-        "INTU", "GS", "CAT", "GE", "QCOM", "TXN", "BKNG", "VZ", "AXP", "T", "SPGI", "MS",
+        "INTU", "CAT", "GE", "QCOM", "TXN", "BKNG", "VZ", "AXP", "T", "SPGI",
         "RTX", "HON", "PLTR", "DHR", "CMCSA", "NEE", "BLK", "LOW", "PGR", "AMGN", "PFE",
-        "UNP", "ETN", "AMAT", "TJX", "C", "BX", "BSX", "PANW", "COP", "SYK", "UBER",
+        "UNP", "ETN", "AMAT", "TJX", "BX", "BSX", "PANW", "COP", "SYK", "UBER",
         "BA", "ADP", "VRTX", "ANET", "MU", "FI", "SCHW", "GILD", "BMY", "TMUS", "DE",
         "ADI", "SBUX", "KKR", "MMC", "MDT", "LMT", "CB", "LRCX", "PLD", "UPS", "PYPL",
-        "MO", "GEV", "NKE", "EQIX", "APH", "AMT", "TT", "CRWD", "SO", "INTC", "ICE",
+        "MO", "NKE", "EQIX", "APH", "AMT", "TT", "CRWD", "SO", "INTC", "ICE",
         "CMG", "KLAC", "PH", "ELV", "CDNS", "CME", "SHW", "DUK", "MDLZ", "USB", "SNPS",
         "PNC", "ZTS", "AON", "MSI", "WM", "REGN", "MCO", "CI", "CL", "CEG", "EMR", "WELL",
         "MCK", "ORLY", "ITW", "CTAS", "TDG", "MMM", "EOG", "COF", "GD", "APD", "MAR",
         "NOC", "WMB", "BDX", "ADSK", "FDX", "CSX", "ECL", "AJG", "HLT", "FTNT", "TGT",
-        "TFC", "CARR", "OKE", "PCAR", "GM", "CVS", "FCX", "BK", "ROP", "ABNB", "RCL",
+        "TFC", "CARR", "OKE", "PCAR", "GM", "CVS", "FCX", "ROP", "ABNB", "RCL",
         "HCA", "AZO", "DLR", "SLB", "JCI", "TRV", "SRE", "SPG", "NXPI", "NSC", "AMP",
         "CPRT", "FICO", "AFL", "ALL", "KMI", "URI", "GWW", "PWR", "CMI", "ROST", "VST",
         "PSA", "AEP", "MET", "MSCI", "PSX", "O", "AXON", "AIG", "MPC", "HWM", "NEM",
         "TEL", "FIS", "PAYX", "D", "LULU", "FAST", "EW", "DFS", "KMB", "AME", "DHI",
-        "PRU", "PEG", "PCG", "RSG", "KVUE", "LHX", "CCI", "CBRE", "BKR", "KR", "CTVA",
+        "PRU", "PEG", "PCG", "RSG", "LHX", "CCI", "CBRE", "BKR", "KR", "CTVA",
         "IR", "TRGP", "A", "COR", "VRSK", "VLO", "CTSH", "DAL", "F", "SYY", "IT", "OTIS",
         "YUM", "XEL", "EA", "HES", "ODFL", "KDP", "GLW", "MNST", "CHTR", "LEN", "GEHC",
         "VMC", "GIS", "STZ", "EXC", "WAB", "IDXX", "RMD", "DELL", "ACGL", "IQV", "EFX",
@@ -55,7 +59,7 @@ class DadosBaseYahoo(DadosBase):
         "BG", "PAYC", "EMN", "AIZ", "IPG", "INCY", "UHS", "NWSA", "ERIE", "FOXA", "TAP",
         "GNRC", "CRL", "LKQ", "PNW", "ENPH", "GL", "SOLV", "HSIC", "HRL", "AES", "RL",
         "MKTX", "AOS", "FRT", "CPB", "WYNN", "TFX", "MTCH", "MOS", "MGM", "IVZ", "APA",
-        "BF.B", "HAS", "CZR", "HII", "BWA", "CE", "WBA", "DVA", "BEN", "QRVO", "MHK",
+        "BF-B", "HAS", "CZR", "HII", "BWA", "CE", "WBA", "DVA", "BEN", "QRVO", "MHK",
         "PARA", "FMC", "FOX", "NWS", "AMTM"
     ]
 
@@ -63,14 +67,27 @@ class DadosBaseYahoo(DadosBase):
     def __init__(self):
         super().__init__()
 
+
     @property
-    def df(self) -> pd.DataFrame:
+    def _construir_base_de_dados(self, path_base_dados: str) -> pd.DataFrame:        
+        # Criar DataFrame da base de dados completa
         scores = []
-        for ticker in self.EMPRESAS:
+        num_empresas = len(self.EMPRESAS)
+        for n, ticker in enumerate(self.EMPRESAS):
             empresa = ColetorFinanceiroYahoo(ticker)
-            print(f"[{datetime.datetime.now()}] Calculando score da empresa '{empresa.NOME_EMPRESA}'")
+            print(f"[{datetime.datetime.now()}] Calculando score da empresa '{empresa.NOME_EMPRESA}' ({n+1}/{num_empresas})")
 
-            score = ColetorFinanceiroYahoo(ticker).score_financeiro
-            scores.append(score)
+            try:
+                score = ColetorFinanceiroYahoo(ticker).score_financeiro
+                scores.append(score)
+            except KeyError:
+                print(f"[{datetime.datetime.now()}] EBITDA não encontrado (instituição financeira?)")
+            except AttributeError:
+                print(f"[{datetime.datetime.now()}] Histórico de preços muito pequeno")
 
-        return pd.concat(scores, axis=0)
+        # Escrever base de dados em um arquivo CSV
+        df = pd.concat(scores, axis=0)
+        df.to_csv(path_base_dados, index=False)
+
+        # Retornar base criada
+        return df
